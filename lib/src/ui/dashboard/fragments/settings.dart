dd6290b6fe96967474baae255166d3c1cca40e3f
sobanarshad85 <sobanarshad85@gmail.com>
2019-06-18 16:08:05 +0500

    added tab navigator in application

--- lib/src/ui/dashboard/fragments/settings.dart
+++ lib/src/ui/dashboard/fragments/settings.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:my_movies/src/ui/dashboard/fragments/home.dart';
 
-class Settings extends StatefulWidget {
-  @override
-  _SettingsScreen createState() => new _SettingsScreen();
-}
 
-class _SettingsScreen extends State<Settings> {
+class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Container(
-        child: new Center(
-          child: new Text("Settings"),
+    return Scaffold(
+
+     body: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.home)),
+              ],
+            ),
+            // title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+              Home(),
+            ],
+          ),
         ),
       ),
     );
