f7c68a8cc0061176e3e6a0134449cbea875e6d74
sobanarshad85 <sobanarshad85@gmail.com>
2019-06-18 18:19:35 +0500

    added bottom tab navttom tab navigation in flutter app

--- lib/src/ui/dashboard/fragments/about.dart
+++ lib/src/ui/dashboard/fragments/about.dart
@@ -1,17 +1,67 @@
 import 'package:flutter/material.dart';
 
-class About extends StatefulWidget {
-  @override
-  _AboutScreen createState() => new _AboutScreen();
-}
+// class About extends StatefulWidget {
+//   @override
+//   _AboutScreen createState() => new _AboutScreen();
+// }
 
-class _AboutScreen extends State<About> {
+// class _AboutScreen extends State<About> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       body: new Container(
+//         child: new Center(
+//           child: new Text("About"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return new Scaffold(
-      body: new Container(
-        child: new Center(
-          child: new Text("About"),
+      // color: Colors.yellow,
+      body: DefaultTabController(
+        length: 4,
+        child: new Scaffold(
+          body: TabBarView(
+            children: [
+              new Container(
+                color: Colors.yellow,
+              ),
+              new Container(color: Colors.orange,),
+              new Container(
+                color: Colors.lightGreen,
+              ),
+              new Container(
+                color: Colors.red,
+              ),
+            ],
+          ),
+          bottomNavigationBar: new TabBar(
+            tabs: [
+              Tab(
+                icon: new Icon(Icons.home),
+              ),
+              Tab(
+                icon: new Icon(Icons.rss_feed),
+              ),
+              Tab(
+                icon: new Icon(Icons.perm_identity),
+              ),
+              Tab(icon: new Icon(Icons.settings),)
+            ],
+            labelColor: Colors.yellow,
+            unselectedLabelColor: Colors.blue,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.red,
+          ),
+          backgroundColor: Colors.black,
         ),
       ),
     );
