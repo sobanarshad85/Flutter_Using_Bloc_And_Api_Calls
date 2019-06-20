00a5c7506a7ae60e37376090254acca87321d662
sobanarshad85 <sobanarshad85@gmail.com>
2019-06-20 15:10:06 +0500

    added communication with native android to flutter on contact screen

--- android/app/src/main/java/com/sagar/mymovies/MainActivity.java
+++ android/app/src/main/java/com/sagar/mymovies/MainActivity.java
@@ -1,13 +1,50 @@
 package com.sagar.mymovies;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+import java.util.Map;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "demo.gawkat.com/info";
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+      @Override
+      public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+
+        final Map<String, Object> arguments = methodCall.arguments();
+
+        if (methodCall.method.equals("getMessage")) {
+
+                    String from = (String) arguments.get("from");
+
+                    String message = "Android say hi " + from;
+
+                    result.success(message);
+
+
+//          openWebPage("https://flutter.io");
+        }
+
+      }
+    });
   }
+//  public void openWebPage(String url) {
+//    Uri webpage = Uri.parse(url);
+//    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+//    if (intent.resolveActivity(getPackageManager()) != null) {
+//      startActivity(intent);
+//    }
+//  }
 }
--- lib/src/ui/dashboard/fragments/contact.dart
+++ lib/src/ui/dashboard/fragments/contact.dart
@@ -1,19 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Contact extends StatefulWidget {
+
+class Contact extends StatelessWidget {
   @override
-  _ContactScreen createState() => new _ContactScreen();
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: new MyHomePage(),
+    );
+  }
 }
 
-class _ContactScreen extends State<Contact> {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = const MethodChannel('demo.gawkat.com/info');
+
+  String _message = "No messages yet...";
+
+  @override
+  void initState() {
+    // fetch and change the message from the platform
+
+    _getMessage().then((String message) {
+      setState(() {
+       _message = message;
+      });
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Container(
-        child: new Center(
-          child: new Text("Contact"),
-        ),
+      
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new Text(_message),
+          )
+        ],
       ),
     );
+  }
+
+  Future<String> _getMessage() async {
+    var sendMap = <String, dynamic> {
+      'from' : 'Soban',
+    };
+
+    String value;
+
+    try {
+      value = await platform.invokeMethod('getMessage', sendMap);
+    } catch (e) {
+      print(e);
+    }
+
+    return value;
   }
 }
