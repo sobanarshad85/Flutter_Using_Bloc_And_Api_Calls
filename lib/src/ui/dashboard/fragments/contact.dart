807a9deba19cec48aec974b5987cb8dda50e466d
sobanarshad85 <sobanarshad85@gmail.com>
2019-06-26 16:54:42 +0500

    remove debug banner from contact screen

--- lib/src/ui/dashboard/fragments/contact.dart
+++ lib/src/ui/dashboard/fragments/contact.dart
@@ -6,6 +6,7 @@ class Contact extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: new ThemeData(
         primarySwatch: Colors.lightGreen,
